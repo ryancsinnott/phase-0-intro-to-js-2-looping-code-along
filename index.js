20bdf10a8c99f01fd4714a038a00a1e7bc15ffdf
Ryan Sinnott <ryan.christopher.sinnott@gmail.com>
2023-04-25 15:37:50 -0400

    SemiComplete Looping Lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    debugger;
+  }
+
+  for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    debugger;
+  }
